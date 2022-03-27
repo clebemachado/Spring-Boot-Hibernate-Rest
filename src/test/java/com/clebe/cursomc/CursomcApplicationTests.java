b9fa0b33af877f382d9f9bec2e0951bb80385e39
clebemachado <clebmachado.em@gmail.com>
2022-03-27 10:20:22 -0300

    Iniciando aplicação spring

---
+++ src/test/java/com/clebe/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clebe.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
